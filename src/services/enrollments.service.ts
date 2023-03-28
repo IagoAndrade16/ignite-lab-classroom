4e3c9dd4ed886c0573a9c1e2afd28390ecff5880
Iago Alexandre <iagoaap@outlook.com>
2023-03-28 06:10:19 -0300

    get course by user id

--- src/services/enrollments.service.ts
+++ src/services/enrollments.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 
+interface GetByCourseAndStudentIdParams {
+  courseId: string;
+  studentId: string;
+}
+
 @Injectable()
 export class EnrollmentsService {
   constructor(private prisma: PrismaService) {}
@@ -24,6 +29,19 @@ export class EnrollmentsService {
       },
       orderBy: {
         createdAt: 'desc',
+      },
+    });
+  }
+
+  getByCourseAndStudentId({
+    courseId,
+    studentId,
+  }: GetByCourseAndStudentIdParams) {
+    return this.prisma.enrollment.findFirst({
+      where: {
+        courseId,
+        studentId,
+        canceledAt: null,
       },
     });
   }
